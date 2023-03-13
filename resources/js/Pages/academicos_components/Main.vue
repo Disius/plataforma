1ffbe0b9305340bd3c15a2f6f788fd6b2ffc9a69
Daniel Fernández <defp_99@hotmail.com>
2023-03-12 18:56:52 -0600

    Nueva version

---
+++ resources/js/Pages/academicos_components/Main.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Academicos, aca sera el menu donde iran todos los componentes que se ejecutaran para todos los academicos, pocas palabras todos los componentes relacionados</h1>
+    
+</template>
